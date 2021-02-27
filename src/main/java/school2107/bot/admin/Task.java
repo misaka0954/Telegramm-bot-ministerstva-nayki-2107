aa94ae53605d1d10b7ba4a6f0e2f4457b93f62c5
Misaka <misakachan2545@gmail.com>
2021-02-27 11:12:03 +0300

    Документация

--- src/main/java/school2107/bot/admin/Task.java
+++ src/main/java/school2107/bot/admin/Task.java
@@ -3,35 +3,71 @@ package school2107.bot.admin;
 import java.util.Date;
 
 public class Task {
-    //дата выполнения
+    /**
+     * дата выполнения
+     */
     public Date eventTime;
-    //текст сообщения
+    /**
+     * Текст сообщения
+     */
     public String message;
-    //огэ егэ олимпиада дайджест
+    /**
+     * категория события
+     */
     public String eventCategory;
-    //уровень\\по умолчанию 0;значения 1-3
-    public int level=0;
-    //предмет\\по умолчаию null
-    public String subject=null;
-    //версия для дайджеста
-    public Task(Date date,String message,String cat){
-        this.eventTime=date;
-        this.message=message;
-        this.eventCategory=cat;
+    /**
+     * Уровень события(для олимпиад)
+     * от 1 до 3
+     * в прочих случаях 0
+     */
+    public int level = 0;
+    /**
+     * Предмет события, по умолчанию null
+     */
+    public String subject = null;
+
+    /**
+     * Коструктор для дайджеста
+     *
+     * @param date    дата выполняния
+     * @param message сообщение события
+     * @param cat     категория события
+     */
+    public Task(Date date, String message, String cat) {
+        this.eventTime = date;
+        this.message = message;
+        this.eventCategory = cat;
     }
-    //версия огэ егэ
-    public Task(Date date,String message,String cat,String subject){
-        this.eventTime=date;
-        this.message=message;
-        this.eventCategory=cat;
-        this.subject=subject;
+
+    /**
+     * Версия для ЕГЭ и ОГЭ
+     *
+     * @param date    дата вызова
+     * @param message сообщение
+     * @param cat     категория события
+     * @param subject предмет
+     */
+    public Task(Date date, String message, String cat, String subject) {
+        this.eventTime = date;
+        this.message = message;
+        this.eventCategory = cat;
+        this.subject = subject;
     }
-    //версия для олимпиады
-    public Task(Date date,String message,String cat,int lvl,String subject){
-        this.eventTime=date;
-        this.message=message;
-        this.eventCategory=cat;
-        this.subject=subject;
-        this.level=lvl;
+
+    /**
+     * Версия для олимпиады
+     *
+     * @param date    дата вызова
+     * @param message текст сообщения
+     * @param cat     категория
+     * @param lvl     уровень(1-3)
+     * @param subject предмет
+     */
+    public Task(Date date, String message, String cat, int lvl, String subject) {
+        this.eventTime = date;
+        this.message = message;
+        this.eventCategory = cat;
+        this.subject = subject;
+        this.level = lvl;
     }
 }
